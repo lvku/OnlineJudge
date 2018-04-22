d001a0020bea178575e078514163b972d6e4c784
beijingbeef <hongmingli1988@gmail.com>
2018-04-22 01:05:39 -0700

    add vip

--- account/models.py
+++ account/models.py
@@ -41,6 +41,7 @@ class User(AbstractBaseUser):
     open_api = models.BooleanField(default=False)
     open_api_appkey = models.CharField(max_length=32, null=True)
     is_disabled = models.BooleanField(default=False)
+    is_vip = models.BooleanField(default=False)
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
